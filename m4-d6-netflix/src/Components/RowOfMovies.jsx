4e0982f6588dae18a69c6b77026f07d049a3b9fd
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-08-31 13:26:37 +0100

    Added Row of Movies

---
+++ m4-d6-netflix/src/Components/RowOfMovies.jsx
@@ -0,0 +1,17 @@
+import {Component} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row} from ""
+
+
+
+class RowOfMovies extends Component{
+    render(){
+        return (
+            <Row>
+
+            </Row>
+
+        )
+    }
+}
+export default RowOfMovies
